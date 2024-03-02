1816811bbbedf55d2e224c3bb05521b10585c07e
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-03-02 15:44:24 +0300

    end-up with logic that allow to collect + save images

--- parsing.py
+++ parsing.py
@@ -1,26 +1,39 @@
 import json
 
-from utils import get_favicon
+from utils import parse_html_to_scratch_favicon, save_favicon_as_png
 
 filename_full = 'resources/ship24_couriers_response.json'
 filename_head = 'resources/ship24_couriers_response_head.json'
+filename_problem = 'resources/ship24_couriers_response_problem.json'
 
-f = open(filename_head)
+f = open(filename_full)
 data = json.load(f)
 couriers: list = data['data']['couriers']
 
+
 total_websites = 0
 icon_collected = 0
+errors: list[str] = []
+swift_code: dict[str, str] = {}
+
 for courier in couriers:
     website = courier['website']
     total_websites += 1
     if website:
         courierName = courier['courierName']
-        icon = get_favicon(website)
-        print(f"website: {website}")
+        icon = parse_html_to_scratch_favicon(website)
         print(f"courierName: {courierName}")
-        print(f"icon: {icon}")
-        icon_collected += 1
+        print(f"icon: {icon}\n")
+        if icon:
+            isSaved = save_favicon_as_png(icon, courierName)
+            if isSaved:
+                swift_code[courierName] = courierName
+                icon_collected += 1
+            else:
+                errors.append(courierName)
 
-print(total_websites)
-print(icon_collected)
+print('total: ', total_websites)
+print('collected: ', icon_collected)
+print(errors)
+print(swift_code)
+
--- utils.py
+++ utils.py
@@ -1,20 +1,59 @@
+from __future__ import annotations
 from bs4 import BeautifulSoup
+from PIL import Image
 import requests
 
+_DEFAULT_TIMEOUT = 30
 
-def get_favicon(domain) -> str:
-    # if 'http' not in domain:
-    #     domain = 'http://' + domain
+
+def parse_html_to_scratch_favicon(domain: str) -> str | None:
     try:
-        page = requests.get(domain)
-        soup = BeautifulSoup(page.text, features="lxml")
+        page = requests.get(domain, timeout=_DEFAULT_TIMEOUT)
+        if page.status_code == 200:
+            soup = BeautifulSoup(page.text, features="lxml")
 
-        icon_link = soup.find("link", rel="shortcut icon")
-        if icon_link is None:
-            icon_link = soup.find("link", rel="icon")
-        if icon_link is None:
-            return domain + '/favicon.ico'
+            icon_link = soup.find("link", rel="shortcut icon")
+            if icon_link is None:
+                icon_link = soup.find("link", rel="icon")
+            if icon_link is None:
+                return None
 
-        return icon_link["href"]
+            icon_link = icon_link["href"]
+            if 'http' not in icon_link:
+                if icon_link[0] == '/':
+                    return domain + icon_link[1:]
+                else:
+                    return domain + icon_link
+            return icon_link
     except Exception as e:
-        return  ""
+        print(e)
+        return None
+
+
+def save_favicon_as_png(domain: str, courier_name: str) -> bool:
+    try:
+        format = domain[-3:]
+        filename = f"resources/icons/{courier_name}.{format}"
+        with open(filename, 'wb') as handle:
+            response = requests.get(
+                domain, stream=True,
+                timeout=_DEFAULT_TIMEOUT
+            )
+
+            if not response.ok:
+                print(response)
+
+            for block in response.iter_content(1024):
+                if not block:
+                    break
+
+                handle.write(block)
+
+        if format == 'ico':
+            img = Image.open(filename)
+            img.save(f'resources/icons/{courier_name}.png')
+
+        return True
+    except Exception as e:
+        print(e)
+        return False
